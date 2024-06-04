a6638a964858fb1165a6f25ff6dcbf0d622c699c
mirazeng <zeng.wenqi@northeastern.edu>
2024-06-04 17:19:47 -0400

    A2 bugs need to be fixed

---
+++ src/Kanbas/Courses/Grades/GradesControls.tsx
@@ -0,0 +1,35 @@
+import {FaFileExport, FaFileImport} from "react-icons/fa";
+import {FaGear} from "react-icons/fa6";
+
+export default function GradesControls() {
+    return (
+        <div id="wd-grades-controls" className="float-end text-nowrap">
+            <button id="wd-file-import"
+                    className="btn btn-secondary text-nowrap me-2">
+                <FaFileImport className="me-1 fs-5"/>
+                Import
+            </button>
+
+            <div className="dropdown d-inline me-2">
+                <button id="wd-file-export"
+                        className="btn btn-secondary
+                                   dropdown-toggle"
+                        type="button"
+                        data-bs-toggle="dropdown">
+                    <FaFileExport className="me-1 fs-5"/>
+                    Export
+                </button>
+                {/*<ul className="dropdown-menu">*/}
+                {/*    <a id="wd-export-grades" className="dropdown-item" href="#">*/}
+                {/*        <FaFileExport className="me-2"/>*/}
+                {/*        Export Grades*/}
+                {/*    </a>*/}
+                {/*</ul>*/}
+            </div>
+
+            <button className="btn btn-secondary justify-content-around">
+                <FaGear className="me-1 fs-5"/>
+            </button>
+        </div>
+    );
+}
