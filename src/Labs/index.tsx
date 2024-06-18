4a727a5d722d1c29cde9b57b7c82c85524241f88
mirazeng <zeng.wenqi@northeastern.edu>
2024-06-18 10:53:12 -0400

    Revert "sync branches and re-init a3"
    This reverts commit f5b4257f94fe5dac26c2196fbe6765c890736da8.

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,3 +1,4 @@
+// Wenqing (Miranda) Zeng
 
 import Lab1 from "./Lab1";
 import {Route, Routes, Navigate} from "react-router";
@@ -7,7 +8,7 @@ import Lab3 from "./Lab3";
 
 export default function Labs() {
     return (
-        <div className="p-3">
+        <div>
             <h1>Labs</h1>
             <div>
                 <h3>
@@ -20,7 +21,7 @@ export default function Labs() {
                 <Route path="/" element={<Navigate to="Lab1"/>}/>
                 <Route path="Lab1" element={<Lab1/>}/>
                 <Route path="Lab2" element={<Lab2/>}/>
-                <Route path="Lab3/*" element={<Lab3/>}/>
+                <Route path="Lab3" element={<Lab3/>}/>
             </Routes>
         </div>
     );
