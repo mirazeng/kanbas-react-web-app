86dace9adaa805e0cd7c2c0259ba5762961a38cd
mirazeng <zeng.wenqi@northeastern.edu>
2024-06-18 10:58:24 -0400

    migrated change to a3 branch and re-push

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,7 +1,80 @@
+import VariablesAndConstants from "./VariablesAndConstants";
+import VariableTypes from "./VariableTypes";
+import BooleanVariables from "./BooleanVariables";
+import IfElse from "./IfElse";
+import TernaryOperator from "./TernaryOperator";
+import ConditionalOutputIfElse from "./ConditionalOutputIfElse";
+import ConditionalOutputInline from "./ConditionalOutputInline";
+import LegacyFunctions from "./LegacyFunctions";
+import ArrowFunctions from "./ArrowFunctions";
+import ImpliedReturn from "./ImpliedReturn";
+import TemplateLiterals from "./TemplateLiterals";
+import SimpleArrays from "./SimpleArrays";
+import ArrayIndexAndLength from "./ArrayIndexAndLength";
+import AddingAndRemovingToFromArrays from "./AddingAndRemovingToFromArrays";
+import Forloops from "./ForLoops";
+import MapFunction from "./MapFunction";
+import FindFunction from "./FindFunction";
+import FindIndexFunction from "./FindIndexFunction";
+import FilterFunction from "./FilterFunction";
+import JsonStringify from "./JsonStringify";
+import House from "./House";
+import TodoItem from "./todos/TodoItem";
+import TodoList from "./todos/TodoList";
+import Spreading from "./Spreading";
+import Destructing from "./Destructing";
+import FunctionDestructing from "./FunctionDestructing";
+import Classes from "./Classes";
+import DestructingImports from "./DestructingImports";
+import Styles from "./Styles";
+import Add from "./Add";
+import Square from "./Square";
+import Highlight from "./Highlight";
+import AddPathParameters from "./AddPathParameters";
+import PathParameters from "./PathParameters";
+
+
 export default function Lab3() {
     return (
-        <div>
-            <h2>Lab 3</h2>
+        <div id="wd-lab3" className="container-fluid">
+            <h3>Lab 3</h3>
+            <VariablesAndConstants/>
+            <VariableTypes/>
+            <BooleanVariables/>
+            <IfElse/>
+            <TernaryOperator/>
+            <ConditionalOutputIfElse/>
+            <ConditionalOutputInline/>
+            <LegacyFunctions/>
+            <ArrowFunctions/>
+            <ImpliedReturn/>
+            <TemplateLiterals/>
+            <SimpleArrays/>
+            <ArrayIndexAndLength/>
+                <AddingAndRemovingToFromArrays/>
+            <Forloops/>
+            <MapFunction/>
+            <FindFunction/>
+                <FindIndexFunction/>
+            <FilterFunction/>
+            <JsonStringify/>
+            <House/>
+            <TodoItem/>
+            <TodoList/>
+            <Spreading/>
+                <Destructing/>
+            <FunctionDestructing/>
+            <DestructingImports/>
+            <Classes/>
+                <Styles/>
+            <Add a={3} b={4}/>
+            <h4>Square of 4</h4>
+            <Square>4</Square>
+            <hr/>
+            <Highlight>     Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipitratione eaque illo minus cum, saepe totam
+                vel nihil repellat nemo explicabo excepturi consectetur. Modi omnis minus sequi maiores, provident voluptates.
+            </Highlight>
+                <PathParameters/>
         </div>
     );
 }
--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -46,7 +46,7 @@ export default function TOC() {
                 </a>
             </li>
             <li className="nav-item">
-                <a id="wd-k" href="https://github.com/mirazeng/kanbas-react-web-app" className="nav-link">
+                <a id="wd-github" href="https://github.com/mirazeng/kanbas-react-web-app" className="nav-link">
                     Miranda's GitHub
                 </a>
             </li>
