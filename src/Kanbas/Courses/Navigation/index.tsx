f8f45d4fd93117dc43fd0707c25cc1577f4ea757
mirazeng <zeng.wenqi@northeastern.edu>
2024-06-18 22:30:23 -0400

    before 3.7

--- src/Kanbas/Courses/Navigation/index.tsx
+++ src/Kanbas/Courses/Navigation/index.tsx
@@ -1,29 +1,66 @@
 import "./index.css";
+import {Link, useLocation, useParams} from "react-router-dom";
 
 export default function CoursesNavigation() {
+    const {pathname} = useLocation();
+    const {cid} = useParams();
+
+    const links = [
+        {label: "Home", path: `/Kanbas/Courses/${cid}/Home`},
+        {label: "Modules", path: `/Kanbas/Courses/${cid}/Modules`},
+        {label: "Piazza", path: `/Kanbas/Courses/${cid}/Piazza`},
+        {label: "Zoom", path: `/Kanbas/Courses/${cid}/Zoom`},
+        {label: "Assignments", path: `/Kanbas/Courses/${cid}/Assignments`},
+        {label: "Quizzes", path: `/Kanbas/Courses/${cid}/Quizzes`},
+        {label: "Grades", path: `/Kanbas/Courses/${cid}/Grades`},
+    ];
     return (
         <div id="wd-courses-navigation" className="list-group fs-5 rounded-0">
 
-            <a id="wd-courses-home-link" href="#/Kanbas/Courses/1234/Home"
-            className="list-group-item active border border-0">Home</a>
+            {links.map((link) => (
+                <Link key={link.path} to={link.path}
+                      className={`list-group-item border-0 text-danger 
+                                  ${pathname.includes(link.label) 
+                          ? "text-black bg-white" : "text-danger bg-white"}`}>
+                    <br/>
+                    {link.label}
+                </Link>
+            ))}
 
-            <a id="wd-courses-modules-link" href="#/Kanbas/Courses/1234/Modules"
-            className="list-group-item text-danger border border-0">Modules</a>
+            {/*<a id="wd-courses-home-link" href="#/Kanbas/Courses/1234/Home"*/
+            }
+            {/*className="list-group-item active border border-0">Home</a>*/
+            }
 
-            <a id="wd-courses-piazza-link" href="#/Kanbas/Courses/1234/Piazza"
-            className="list-group-item text-danger border border-0">Piazza</a>
+            {/*<a id="wd-courses-modules-link" href="#/Kanbas/Courses/1234/Modules"*/
+            }
+            {/*className="list-group-item text-danger border border-0">Modules</a>*/
+            }
 
-            <a id="wd-courses-zoom-link" href="#/Kanbas/Courses/1234/Zoom"
-            className="list-group-item text-danger border border-0">Zoom</a>
+            {/*<a id="wd-courses-piazza-link" href="#/Kanbas/Courses/1234/Piazza"*/
+            }
+            {/*className="list-group-item text-danger border border-0">Piazza</a>*/
+            }
 
-            <a id="wd-courses-assignment-link" href="#/Kanbas/Courses/1234/Assignments"
-            className="list-group-item text-danger border border-0">Assignments</a>
+            {/*<a id="wd-courses-zoom-link" href="#/Kanbas/Courses/1234/Zoom"*/
+            }
+            {/*className="list-group-item text-danger border border-0">Zoom</a>*/
+            }
 
-            <a id="wd-courses-quizzes-link" href="#/Kanbas/Courses/1234/Quizzes"
-            className="list-group-item text-danger border border-0">Quizzes</a>
+            {/*<a id="wd-courses-assignment-link" href="#/Kanbas/Courses/1234/Assignments"*/
+            }
+            {/*className="list-group-item text-danger border border-0">Assignments</a>*/
+            }
 
-            <a id="wd-courses-grades-link" href="#/Kanbas/Courses/1234/Grades"
-            className="list-group-item text-danger border border-0">Grades</a>
+            {/*<a id="wd-courses-quizzes-link" href="#/Kanbas/Courses/1234/Quizzes"*/
+            }
+            {/*className="list-group-item text-danger border border-0">Quizzes</a>*/
+            }
+
+            {/*<a id="wd-courses-grades-link" href="#/Kanbas/Courses/1234/Grades"*/
+            }
+            {/*className="list-group-item text-danger border border-0">Grades</a>*/
+            }
         </div>
     );
 }
--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -1,18 +1,27 @@
+import {courses} from "../Database";
 import CoursesNavigation from "./Navigation";
 import Modules from "./Modules";
 import Home from "./Home";
 import Assignments from "./Assignments";
 import AssignmentEditor from "./Assignments/Editor";
-import {Navigate, Route, Routes} from "react-router";
+import {Navigate, Route, Routes, useParams, useLocation} from "react-router";
 import {FaAlignJustify} from "react-icons/fa";
 import Grades from "./Grades";
 
 export default function Courses() {
+
+    const {cid} = useParams();
+    const course = courses.find((course) => course._id === cid);
+    const {pathname} = useLocation();
+
     return (
         <div id="wd-courses">
-            <h2 className="text-danger"><FaAlignJustify className="me-4 fs-4 mb-1"/>
-                Course 1234</h2>
+            <h2 className="text-danger">
+                <FaAlignJustify className="me-4 fs-4 mb-1"/>
+                {course && course.name} &gt; {pathname.split("/")[4]}
+            </h2>
             <hr/>
+
             <div className="d-flex">
                 <div className="d-none d-md-block">
                     <CoursesNavigation/>
