5bc19eb821bd98c699927289b827837028878399
mirazeng <zeng.wenqi@northeastern.edu>
2024-06-23 15:17:10 -0400

    updated grades page with matching assignment

--- src/Kanbas/Courses/Grades/index.tsx
+++ src/Kanbas/Courses/Grades/index.tsx
@@ -4,118 +4,133 @@ import {FiFilter} from "react-icons/fi";
 import {MdOutlineKeyboardArrowDown} from "react-icons/md";
 import * as db from "../../Database";
 import {useParams} from "react-router";
+import {assignments} from "../../Database";
 
 export default function Grades() {
-    const {cid} = useParams();
+    const {cid} = useParams(); //current active course id
+    const assignment = db.assignments;
     const enrollments = db.enrollments;
     const users = db.users;
     const grades = db.grades;
 
+
     const getGradesData = (enrollments: any[], users: any[], grades: any[]) => {
         let result = []; //
 
-        let enrolledStudents = enrollments.filter((enrollment) => enrollment.course === cid);
-
-        console.log("DEBUG: enrolled students: ", enrolledStudents);
+        // we need to know which students is in current active class
+        let studentEnrolledClass = enrollments.filter((dataEntry) => dataEntry.course === cid);
+        console.log("Debug: test is ", studentEnrolledClass);
 
         // For each student enrolled, use the "user" field to find matching entries from grades.json and users.json
-        for (let i = 0; i < enrolledStudents.length; i++) {
-            let enrolled = enrolledStudents[i];
-            let enrolledID = enrolled.user;
+        for (let i = 0; i < studentEnrolledClass.length; i++) {
+            let eachStudent = studentEnrolledClass[i];
+            let enrolledID = eachStudent.user;
+
             let enrolledFirstName = users.find((user) => user._id === enrolledID).firstName;
             let enrolledLastName = users.find((user) => user._id === enrolledID).lastName;
             let studentName = enrolledFirstName + " " + enrolledLastName;
 
-            let studentGrades = grades.filter((grade) => grade.student === enrolledID);
+            let studentGrades = grades.filter((gradeData) => {
+                return (gradeData.student === enrolledID) && (() => assignment.find((entry) => entry.course === cid));
+            });
 
             let studentData = {
-                studentID: enrolledID, studentName: studentName, grades: studentGrades
+                studentID: enrolledID,
+                studentName: studentName,
+                studentGrades: studentGrades
             };
 
             result.push(studentData);
         }
-        return result;
-        console.log("DEBUG: result: ", result);
+        return result
     }
 
     const target_data = getGradesData(enrollments, users, grades);
 
 
     return (<div id="wd-grades">
-            <br/>
-            <GradesControls/>
-            <br/> <br/>
-            <div className="row mb-3">
-                <div className="col mb-3">
-                    <label htmlFor="wd-search-student-name" className="form-label fw-bold fs-5">
-                        Student Names
-                    </label>
-                    <div className="input-group">
-                        {/*/!*<span className="input-group-text">*/}
-                        {/*        <i className="bi bi-search"/>*/}
-                        {/*    </span>*!/*/}
-                        {/*/!*<select className="form-select input-group-text"*!/*/}
-                        {/*/!*        defaultValue=""*!/*/}
-                        {/*/!*style={{textAlign:'left', paddingLeft:'12px'}}>*!/*/}
-                        {/*/!*    <option value="" disabled className="float-start">Search Students</option>*!/*/}
-                        {/*/!*</select>*!/*/}
-                        <span className="input-group-text">
+        <br/>
+        <GradesControls/>
+        <br/> <br/>
+        <div className="row mb-3">
+            <div className="col mb-3">
+                <label htmlFor="wd-search-student-name" className="form-label fw-bold fs-5">
+                    Student Names
+                </label>
+                <div className="input-group">
+                    {/*/!*<span className="input-group-text">*/}
+                    {/*        <i className="bi bi-search"/>*/}
+                    {/*    </span>*!/*/}
+                    {/*/!*<select className="form-select input-group-text"*!/*/}
+                    {/*/!*        defaultValue=""*!/*/}
+                    {/*/!*style={{textAlign:'left', paddingLeft:'12px'}}>*!/*/}
+                    {/*/!*    <option value="" disabled className="float-start">Search Students</option>*!/*/}
+                    {/*/!*</select>*!/*/}
+                    <span className="input-group-text">
                                 <HiMagnifyingGlass/>
                             </span>
-                        <input type="text"
-                               className="form-control"
-                               id="wd-search-student-name"
-                               placeholder="Search Students"/>
-                        <span className="input-group-text">
+                    <input type="text"
+                           className="form-control"
+                           id="wd-search-student-name"
+                           placeholder="Search Students"/>
+                    <span className="input-group-text">
                                 <MdOutlineKeyboardArrowDown/>
                         </span>
-                    </div>
                 </div>
-                <div className="col mb-3">
-                    <label htmlFor="wd-search-assignment-name" className="form-label fw-bold fs-5">
-                        Assignment Names
-                    </label>
-                    <div className="input-group">
+            </div>
+            <div className="col mb-3">
+                <label htmlFor="wd-search-assignment-name" className="form-label fw-bold fs-5">
+                    Assignment Names
+                </label>
+                <div className="input-group">
                             <span className="input-group-text">
                                 <HiMagnifyingGlass/>
                             </span>
-                        <input id="wd-search-assignment-name" className="form-control" type="text"
-                               placeholder="Search Assignments"/>
-                        <span className="input-group-text">
+                    <input id="wd-search-assignment-name" className="form-control" type="text"
+                           placeholder="Search Assignments"/>
+                    <span className="input-group-text">
                                 <MdOutlineKeyboardArrowDown/>
                         </span>
-                    </div>
                 </div>
             </div>
+        </div>
 
-            <div className="row mb-3">
-                <div className="col-12 mb-4">
-                    <button type="button" className="btn btn-secondary me-2">
-                        <FiFilter className="me-1"/>
-                        Apply Filters
-                    </button>
-                </div>
+        <div className="row mb-3">
+            <div className="col-12 mb-4">
+                <button type="button" className="btn btn-secondary me-2">
+                    <FiFilter className="me-1"/>
+                    Apply Filters
+                </button>
+            </div>
 
-                <div className="col-12">
-                    <div className="table-responsive-sm">
-                        <table className="table table-striped table-bordered">
-                            <thead>
+            <div className="col-12">
+                <div className="table-responsive-sm">
+                    <table className="table table-striped table-bordered">
+                        <thead>
 
-                            <tr>
-                                <th>Student Name</th>
-                                <th> A101<br/> Out of 100</th>
-                                <th> A102<br/> Out of 100</th>
-                            </tr>
-                            </thead>
+                        <tr>
+                            <th>Student Name</th>
+                            {
+                                (assignments.filter((assignmentEntry) => assignmentEntry.course === cid)).map((classEntry) => (
 
-                            <tbody>
-                            {target_data.map((student) => (<tr>
-                                    <td className={"text-danger"}>{student.studentName}
-                                    </td>
-                                    {student.grades.map((grade) => (<td>{grade.grade}</td>))}
-                                </tr>))}
+                                    (
+                                        <th>{classEntry._id}<br/> Out of 100</th>
+                                    )
+                                ))
 
-                            {/*<tr>
+                            }
+
+                        </tr>
+                        </thead>
+
+                        <tbody>
+                        {target_data.map((studentEntry) => (<tr>
+                            <td className={"text-danger"}>{studentEntry.studentName}
+                            </td>
+                            {studentEntry.studentGrades.map((grade) => (<td>{grade.grade}</td>))}
+                        </tr>))}
+
+                        {/*<tr>
                                 <td className="text-danger">Jane Adams</td>
                                 <td>100%</td>
                                 <td>96.65%</td>
@@ -159,11 +174,11 @@ export default function Grades() {
                                 <td>100%</td>
                                 <td>100%</td>
                             </tr>*/}
-                            </tbody>
-                        </table>
+                        </tbody>
+                    </table>
 
-                    </div>
                 </div>
             </div>
-        </div>);
+        </div>
+    </div>);
 }
